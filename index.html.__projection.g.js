2ad14212d59d71c10ed5b3d75938b269b734a03d
isaiah hendrix <isaiah.hendrix1@outlook.com>
2024-04-17 23:26:21 -0500

    added more stuff to index

---
+++ index.html.__projection.g.js
@@ -0,0 +1,44 @@
+/* BEGIN EXTERNAL SOURCE */
+function name14() {
+sayname()
+}
+/* END EXTERNAL SOURCE */
+/* BEGIN EXTERNAL SOURCE */
+function name15() {
+sayname()
+}
+/* END EXTERNAL SOURCE */
+/* BEGIN EXTERNAL SOURCE */
+function name16() {
+sayname()
+}
+/* END EXTERNAL SOURCE */
+/* BEGIN EXTERNAL SOURCE */
+function name17() {
+sayname()
+}
+/* END EXTERNAL SOURCE */
+/* BEGIN EXTERNAL SOURCE */
+function name18() {
+sayname()
+}
+/* END EXTERNAL SOURCE */
+/* BEGIN EXTERNAL SOURCE */
+function name19() {
+sayname()
+}
+/* END EXTERNAL SOURCE */
+/* BEGIN EXTERNAL SOURCE */
+function name20() {
+sayname()
+}
+/* END EXTERNAL SOURCE */
+/* BEGIN EXTERNAL SOURCE */
+
+    function sayname() {
+        var input = document.getElementById("textbox").value;
+        alert(input);
+    }
+
+
+/* END EXTERNAL SOURCE */
